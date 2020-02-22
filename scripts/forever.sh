8a2ad671b4ec638fe75efb612097818589a1eaec
Sergey Terekhov <Terekhov.S@sberbank.ru>
2020-02-22 20:21:39 +0300

    forever fix

--- scripts/forever.sh
+++ scripts/forever.sh
@@ -1,1 +1,1 @@
-forever start --uid nlp --spinSleepTime 10000 --killSignal=SIGTERM --append -c "nodemon --exitcrash" dist/http.js
+forever start --uid nlp --spinSleepTime 10000 --killSignal=SIGTERM --append -c "nodemon --exitcrash" dist/http/index.js
